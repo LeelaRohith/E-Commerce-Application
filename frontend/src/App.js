3e2061d121d812fa5d9e321003ff683a73ff8d6e
LeelaRohith <leelarohith10@gmail.com>
2024-11-18 20:58:46 +0530

    product page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import { ThemeProvider } from "@emotion/react";
 import Navbar from "./customer/components/Navbar";
 import customeTheme from "./Theme/customeTheme";
 import Home from "./customer/pages/Home/Home";
+import Product from "./customer/pages/Product/Product";
 
 function App() {
   return (
     <ThemeProvider theme={customeTheme}>
       <div>
         <Navbar></Navbar>
-        <Home></Home>
+        {/* <Home></Home> */}
+        <Product />
       </div>
     </ThemeProvider>
   );
